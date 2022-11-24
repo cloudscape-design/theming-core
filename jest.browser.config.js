2bec09c01319c36b6d1e0647715cd6c19c674154
Timo Gasda <2446349+timogasda@users.noreply.github.com>
2022-11-24 14:22:03 +0100

    chore: Merge code coverage reports (#26)

--- jest.browser.config.js
+++ jest.browser.config.js
@@ -7,6 +7,7 @@ module.exports = merge.recursive(tsPreset, {
   testEnvironment: 'jsdom',
   moduleFileExtensions: ['js', 'ts'],
   collectCoverage: true,
+  coverageDirectory: 'coverage/browser',
   globals: {
     'ts-jest': {
       tsconfig: './tsconfig.test.json',
