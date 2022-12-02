13bea4be560bfca9800ddc4e9947a13711998b0e
Michael Dowse <dowsem@amazon.com>
2022-12-02 11:35:10 +0000

    chore: Upgrade to Node16
    - Regenerate package-lock

--- scripts/unlock-packages.js
+++ scripts/unlock-packages.js
@@ -9,9 +9,9 @@ const fs = require('fs');
 const filename = require.resolve('../package-lock.json');
 const packageLock = require(filename);
 
-Object.keys(packageLock.dependencies).forEach((dependency) => {
-  if (dependency.startsWith('@cloudscape-design/')) {
-    delete packageLock.dependencies[dependency];
+Object.keys(packageLock.packages).forEach((dependencyName) => {
+  if (dependencyName.startsWith('node_modules/@cloudscape-design/')) {
+    delete packageLock.dependencies[dependencyName];
   }
 });
 fs.writeFileSync(filename, JSON.stringify(packageLock, null, 2) + '\n');
