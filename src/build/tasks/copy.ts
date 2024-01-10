2eb4509dffa32fe7c322998b8acce57d18d0a7f5
jasmaa <jasonmaa3955@gmail.com>
2024-01-10 00:06:15 -0500

    Use mkdirSync in copy

--- src/build/tasks/copy.ts
+++ src/build/tasks/copy.ts
@@ -1,11 +1,11 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import { execSync } from 'child_process';
-import { existsSync } from 'fs';
+import { existsSync, mkdirSync } from 'fs';
 
 export function copyAllFiles(from: string, to: string) {
   if (!existsSync(to)) {
-    execSync(`mkdir -p ${to}`, { stdio: 'inherit' });
+    mkdirSync(to, { recursive: true });
   }
   execSync(`cp -r ${from}/. ${to}`, { stdio: 'inherit' });
 }
