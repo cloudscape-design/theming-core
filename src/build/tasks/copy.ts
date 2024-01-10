fe862b1792a9934d1bbb7e5b38285c74f3385d2f
jasmaa <jasonmaa3955@gmail.com>
2024-01-10 10:44:57 -0500

    Use cpSync in copy

--- src/build/tasks/copy.ts
+++ src/build/tasks/copy.ts
@@ -1,11 +1,10 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { execSync } from 'child_process';
-import { existsSync, mkdirSync } from 'fs';
+import { cpSync, existsSync, mkdirSync } from 'fs';
 
 export function copyAllFiles(from: string, to: string) {
   if (!existsSync(to)) {
     mkdirSync(to, { recursive: true });
   }
-  execSync(`cp -r ${from}/. ${to}`, { stdio: 'inherit' });
+  cpSync(from, to, { recursive: true });
 }
