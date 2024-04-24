6229f0ebb9cf030a3d7fbeb890c540bb3e654797
Boris Serdiuk <serdiuk@amazon.com>
2024-04-24 11:13:54 +0200

    chore: Explose the entire token set internally (#76)

--- src/build/tasks/__tests__/internal-tokens.test.ts
+++ src/build/tasks/__tests__/internal-tokens.test.ts
@@ -1,24 +1,16 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import { test, expect } from 'vitest';
-import { rootTheme, defaultsResolution, preset } from '../../../__fixtures__/common';
+import { defaultsResolution, preset } from '../../../__fixtures__/common';
 
 import { generateTokensDeclarationFile, generateTokensFile } from '../internal-tokens';
 
 const propertiesMap = preset.propertiesMap;
-const publicTokens = preset.exposed;
 
 test('generateTokensFile matches previous snapshot', () => {
-  expect(generateTokensFile(rootTheme, defaultsResolution, propertiesMap, publicTokens)).toMatchSnapshot();
+  expect(generateTokensFile(defaultsResolution, propertiesMap)).toMatchSnapshot();
 });
 
 test('generateTokensDeclarationFile matches previous snapshot', () => {
-  expect(generateTokensDeclarationFile(publicTokens)).toMatchSnapshot();
-});
-
-test('throws error for missing custom properties', () => {
-  const func = () => {
-    generateTokensFile(rootTheme, defaultsResolution, {}, publicTokens);
-  };
-  expect(func).toThrowError(new Error('Token shadow is not mapped to a CSS Custom Property'));
+  expect(generateTokensDeclarationFile(propertiesMap)).toMatchSnapshot();
 });
